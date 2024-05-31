a02123cf60830e3b5f7cbeb4d1b16bd15936585b
wilder choque quispe <didier.c.q.17@gmail.com>
2024-05-31 16:39:25 -0400

    vista de 3.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,72 +1,63 @@
 import { Routes } from '@angular/router';
 import path from 'path';
 
-
 export const routes: Routes = [
-
-    {
-        path: 'layout',
-        loadComponent: () => import('./shared/components/layout/layout.component'),
-        children: [
-            {
-                path: 'dashboard',
-                /* icon: '', */
-                title: 'Dashboard',
-                loadComponent: () => import('./admin/dashboard/dashboard.component'),
-            },
-            {
-                path: 'administrador',
-                title: 'Administrador',
-                loadComponent: () => import('./admin/administrador/administrador.component'),
-            },
-            {
-                path: 'profesor',
-                title: 'Profesor',
-                loadComponent: () => import('./admin/profesor/profesor.component'),
-            },
-            {
-                path: 'user/:id',
-                title: 'UserView',
-                loadComponent: () => import('./admin/user/user.component'),
-            },
-            {
-                path: 'user-list',
-                title: 'User',
-                loadComponent: () => import('./admin/user/user.component'),
-            },
-            {
-                path: 'materia',
-                title: 'Materia',
-                loadComponent: () => import('./admin/materia/materia.component'),
-            },
-            {
-                path: 'modulo',
-                title: 'Modulo',
-                loadComponent: () => import('./admin/modulo/modulo.component'),
-            },
-            {
-                path: 'carrera',
-                title: 'Carrera',
-                loadComponent: () => import('./admin/carrera/carrera.component'),
-            },
-            {
-                path: 'gestion',
-                title: 'Gestion',
-                loadComponent: () => import('./admin/gestion/gestion.component'),
-            },
-            /*{
+  {
+    path: 'layout',
+    loadComponent: () => import('./shared/components/layout/layout.component'),
+    children: [
+      {
+        path: 'dashboard',
+        /* icon: '', */
+        title: 'Dashboard',
+        loadComponent: () => import('./admin/dashboard/dashboard.component'),
+      },
+      {
+        path: 'user-list',
+        title: 'User',
+        loadComponent: () => import('./admin/user/user.component'),
+      },
+      {
+        path: 'administrador',
+        title: 'Administrador',
+        loadComponent: () =>
+          import('./admin/administrador/administrador.component'),
+      },
+      {
+        path: 'modulo',
+        title: 'Modulo',
+        loadComponent: () => import('./admin/modulo/modulo.component'),
+      },
+      {
+        path: 'carrera',
+        title: 'Carrera',
+        loadComponent: () => import('./admin/carrera/carrera.component'),
+      },
+      {
+        path: 'materia',
+        title: 'Materia',
+        loadComponent: () => import('./admin/materia/materia.component'),
+      },
+      {
+        path: 'profesor',
+        title: 'Profesor',
+        loadComponent: () => import('./admin/profesor/profesor.component'),
+      },
+      {
+        path: 'gestion',
+        title: 'Gestion',
+        loadComponent: () => import('./admin/gestion/gestion.component'),
+      },
+      /*{
                 path: '',
                 redirectTo: 'dashboard',
                 pathMatch: 'full',
             },*/
-
-        ]
-    },
-    {
-        path: '',
-        redirectTo: '/layout',
-        pathMatch: 'full'
-    }
-
-
+    ],
+  },
+  {
+    path: '',
+    redirectTo: '/layout',
+    pathMatch: 'full',
+  },
 ];
